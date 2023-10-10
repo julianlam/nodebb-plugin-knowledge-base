0652440c62ed9a57042096a9ffbbc90f69c4a461
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-10-10 10:15:55 -0400

    feat: update for 3.x
    removed unused stuff, plugin doesnt have admin page or settings

--- library.js
+++ library.js
@@ -1,55 +1,21 @@
 'use strict';
 
-const meta = require.main.require('./src/meta');
 const privileges = require.main.require('./src/privileges');
 const posts = require.main.require('./src/posts');
 const topics = require.main.require('./src/topics');
-const controllers = require('./lib/controllers');
 
 const plugin = module.exports;
 
 plugin.init = function (params, callback) {
-	const router = params.router;
-	const hostMiddleware = params.middleware;
-	// const hostControllers = params.controllers;
-
-	router.get('/admin/plugins/knowledge-base', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/knowledge-base', controllers.renderAdminPage);
-
-	plugin.syncSettings(callback);
-};
-
-plugin.syncSettings = function (callback) {
-	meta.settings.get('knowledge-base', function (err, settings) {
-		if (err) {
-			return callback(err);
-		}
-
-		plugin.settings = Object.assign((plugin.settings || {}), settings);
-		callback();
-	});
-};
-
-plugin.onSettingsChange = function (data) {
-	if (data.plugin === 'knowledge-base') {
-		plugin.settings = Object.assign((plugin.settings || {}), data.settings);
-	}
-};
-
-plugin.addAdminNavigation = function (header, callback) {
-	header.plugins.push({
-		route: '/plugins/knowledge-base',
-		icon: 'fa-tint',
-		name: 'Knowledge Base',
-	});
-
-	callback(null, header);
+	callback();
 };
 
 plugin.addPrivileges = function (data, callback) {
-	const payload = this.hook.endsWith('human') ? { name: 'Knowledge Base' } : 'groups:knowledgeBase';
-	data.push(payload);
-	process.nextTick(callback, null, data);
+	data.privileges.set(
+		'knowledgeBase', { label: 'Knowledge Base' },
+	);
+
+	callback(null, data);
 };
 
 plugin.allowEditing = async function (data) {
